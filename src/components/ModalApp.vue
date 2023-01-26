5ec1c967197656de750d54a256c987fa3c967225
IrynaShvets <86491334+IrynaShvets@users.noreply.github.com>
2023-01-26 11:29:54 +0200

    added change library and markup modal

--- src/components/ModalApp.vue
+++ src/components/ModalApp.vue
@@ -1,6 +1,47 @@
 <template>
-    <div>
-        Modal
+    <div
+      class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-[101] outline-none focus:outline-none justify-center items-center"
+    >
+      <div class="relative w-auto my-6 mx-auto max-w-6xl animate-fade-in">
+        <!--content-->
+        <div
+          class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none bg-gradient-to-l from-pink-200 to-sky-200"
+        >
+          <!--header-->
+          <div
+            class="flex justify-between p-5 border-b border-solid border-slate-200 rounded-t"
+          >
+            <h3 id="modal-title" class="text-3xl font-semibold">Modal Title</h3>
+            <button
+              class="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+              id="close-modal-id"
+            >
+              <svg
+                class="inline-block w-4 h-4 stroke-current stroke-0 fill-black"
+                viewBox="0 0 32 32"
+              >
+                <path
+                  d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <!--body-->
+          <div id="overview-id" class="relative py-3"></div>
+          <!--footer-->
+          <div
+            class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b"
+          >
+            <button
+              id="favorite-btn"
+              class="bg-sky-700 rounded-lg px-4 py-2 uppercase text-white"
+              type="button"
+            >
+              Add to favorite movies
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
--- src/pages/LibraryPage.vue
+++ src/pages/LibraryPage.vue
@@ -1,48 +1,45 @@
 <template>
   <container-app>
-    <main>
+    <main class="pb-20">
       <section>
-        <ul class="grid grid-cols-3 gap-4">
-        <li
-          :id="movie.id"
-          v-for="movie in moviesStore"
-          :key="movie.id"
-          class="relative w-[350px] h-[260px] shadow-[10px_10px_8px_2px_rgba(0,0,0,0.3)] hover:scale-[1.05] hover:transition hover:duration-700 hover:ease-in-out"
-        >
-            <img
-              v-if="movie.backdrop_path"
-              :src="'https://image.tmdb.org/t/p/original' + movie.backdrop_path"
-              :alt="movie.title"
-              loading="lazy"
-              class="mr-6 w-[350px] h-[220px]"
-            />
+        <ul class="grid grid-cols-3 gap-10">
+          <li :id="movie.id" v-for="movie in moviesStore" :key="movie.id"
+            class="relative w-[350px] h-[260px] shadow-[10px_10px_8px_2px_rgba(0,0,0,0.3)] hover:scale-[1.05] hover:transition hover:duration-700 hover:ease-in-out">
+            <img v-if="movie.backdrop_path" :src="'https://image.tmdb.org/t/p/original' + movie.backdrop_path"
+              :alt="movie.title" loading="lazy" class="mr-6 w-[350px] h-[220px]" />
             <p class="absolute top-2 left-2 text-white">
               {{ movie.title }}
             </p>
-          <router-link :to="'/movie/' + movie.id">
-            <svg class="inline-block w-5 h-5 stroke-current stroke-0 fill-gray mr-2" viewBox="0 0 20 20">
-                <path d="M3.828 9l6.071-6.071-1.414-1.414-8.485 8.485 8.485 8.485 1.414-1.414-6.071-6.071h16.172v-2h-16.172z"></path>
-            </svg>
-            <span>Go to full description of movie</span>
-          </router-link>
-          <button @click="deleteFavoriteMovieWithLocalStorage(movie.id)" :data-id="movie.id" type="button" class="w-[350px] h-[40px] p-2 bg-indigo-200 hover:bg-purple-500 text-gray-800 hover:text-white transition-colors">
-            
-            Delete with library
-          </button>
-          <button type="button" class="w-[350px] h-[40px] p-2 bg-indigo-200 hover:bg-purple-500 text-gray-800 hover:text-white transition-colors">
-            Додати до переглянутих
-          </button>
-        </li>
-      </ul>
+            <div class="flex items-center">
+              <router-link :to="'/movie/' + movie.id" class="flex flex-1 items-center p-2 bg-indigo-200 hover:bg-purple-500 text-gray-800 hover:text-white transition-colors">
+                <svg class="inline-block w-5 h-5 stroke-current stroke-0 fill-gray mr-2" viewBox="0 0 20 20">
+                  <path
+                    d="M3.828 9l6.071-6.071-1.414-1.414-8.485 8.485 8.485 8.485 1.414-1.414-6.071-6.071h16.172v-2h-16.172z">
+                  </path>
+                </svg>
+                <span>More info</span>
+              </router-link>
+              <button @click="deleteFavoriteMovieWithLocalStorage(movie.id)" :data-id="movie.id" type="button"
+                class="w-[350px] h-[40px] p-2 bg-indigo-200 hover:bg-purple-500 text-gray-800 hover:text-white transition-colors flex-1">
+
+                Delete
+              </button>
+              <button type="button"
+                class="w-[350px] h-[40px] p-2 bg-indigo-200 hover:bg-purple-500 text-gray-800 hover:text-white transition-colors flex-1">
+                Reviewed
+              </button>
+            </div>
+
+          </li>
+        </ul>
       </section>
-     
     </main>
   </container-app>
 </template>
 
 <script>
 import ContainerApp from "../shared/container/ContainerApp.vue";
-import {  toRaw } from 'vue';
+import { toRaw } from 'vue';
 
 export default {
   name: "LibraryPage",
@@ -59,12 +56,14 @@ export default {
 
   methods: {
     deleteFavoriteMovieWithLocalStorage(currentId) {
-     console.log(currentId)
-   
+      console.log(currentId)
+
     },
   },
 
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
