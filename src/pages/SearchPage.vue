24a19d4a63c8b63b08c251cf32eb296b040e9409
IrynaShvets <86491334+IrynaShvets@users.noreply.github.com>
2023-01-27 14:06:13 +0200

    fix img in search page

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -10,13 +10,12 @@
             class="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-sm border border-slate-300 outline-none focus:outline-none focus:ring w-full" />
         </div>
       </div>
-
       <ul v-if="movies" class="grid grid-cols-3 gap-5">
         <li :id="movie.id" v-for="movie in movies" :key="movie.id"
           class="relative w-[402px] h-auto shadow-[-24px_29px_28px_-4px_rgba(148,153,143,0.75)] hover:scale-[1.02] hover:transition hover:duration-600 hover:ease-in">
           <router-link :to="'/movie/' + movie.id">
-            <img v-if="movie.backdrop_path || movie.backdrop_path !== null"
-              :src="'https://image.tmdb.org/t/p/original' + movie.backdrop_path" alt="movie.title" loading="lazy"
+            <img 
+              :src="'https://image.tmdb.org/t/p/original' + movie.backdrop_path" @error="imageLoadOnError" alt="movie.title" loading="lazy"
               class="rounded w-[402px] h-auto]" />
             <p class="absolute left-2 bottom-2 text-xl text-red-200">
               {{ movie.title }}
@@ -90,6 +89,10 @@ export default {
       this.currentPage = event;
       this.search(this.query, this.currentPage);
     },
+
+    imageLoadOnError(e) {
+          e.target.src = "https://i.gyazo.com/c43bcb8fc7e50c57740731c2c2e301ef.jpg"
+      }
   },
 };
 </script>
